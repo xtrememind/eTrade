3719d73e58a327982b74a5402f1115a56a61a2ee
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-03-21 00:26:22 -0500

    Sadok login controller

--- eTrade/src/main/java/edu/mum/controller/LoginController.java
+++ eTrade/src/main/java/edu/mum/controller/LoginController.java
@@ -1,5 +1,10 @@
 package edu.mum.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-
+ 
 import edu.mum.domain.UserCredentials;
 import edu.mum.service.UserCredentialsService;
 
@@ -20,22 +25,38 @@ public class LoginController {
 	
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String login() {
-		System.out.println("logiiiiin method");
  		return "login";
 	}
  
 	
 	@RequestMapping(value="/postLogin", method = RequestMethod.POST)
-	public String PostLogin(UserCredentials credentials, Model model) {
-		
+	public String PostLogin(UserCredentials credentials, Model model,HttpServletRequest req, HttpServletResponse resp) {
+		 
 		
 		UserCredentials validCredentials = credentialsService.findOne(credentials.getUserName());
 		
-		if (validCredentials == null)
+		if (validCredentials == null || !validCredentials.getPassword().equals(credentials.getPassword()))
 		{
 			model.addAttribute("error", "error") ;
-			return  "login";}
- 
+			return  "login";
+		}
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("userId",validCredentials.getUser().getId());
+		session.setAttribute("fullName",validCredentials.getUser().getFirstName()+" "+validCredentials.getUser().getLastName());
+        String remember = req.getParameter("remember");
+        if(remember!=null && remember.equals("on")){
+            Cookie cookie = new Cookie("userName",req.getParameter("userName"));
+            cookie.setMaxAge(3600*30);
+            resp.addCookie(cookie);
+        }else{
+            for (Cookie cookie : req.getCookies()) {
+                if(cookie.getName().equals("userName")) {
+                    cookie.setMaxAge(0);
+                    resp.addCookie(cookie);
+                }
+            }
+        }
 		model.addAttribute("user", validCredentials.getUser());
  		return "redirect:/index";
 	}
@@ -49,8 +70,9 @@ public class LoginController {
 	}
  
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
-	public String logout(Model model, SessionStatus status) {
-		status.setComplete();
- 		return "redirect:/welcome";
+	public String logout(Model model,HttpServletRequest req) {
+		//status.setComplete();
+		req.getSession().invalidate();
+ 		return "redirect:/login";
  	}
 }
